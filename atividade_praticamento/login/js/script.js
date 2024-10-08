df12ac2baff3daac16394c9f1937f08edbfd5286
RUBENS NETO <121876888+RUBENSNETO0001@users.noreply.github.com>
2024-10-07 21:12:15 -0500

    sadsad

--- atividade_praticamento/login/js/script.js
+++ atividade_praticamento/login/js/script.js
@@ -1,12 +1,11 @@
-function entrar(){
-    $user = document.getElementById("usuario").value;
-    $passoword = document.getElementById("senha").value;
+function enviar(){
+    let user = document.getElementById("user").value;
+    let passoword = document.getElementById("senha").value;
     
-    if($user = "user" && $passoword == "senha"){
-        location.href("paginaPrincipal");
+    if(user == "user" && passoword == "senha"){
+          window.location = "../paginaPrincipal/index.html"
     }
     else{
         alert("Usuario ou senha incorreta");
     }
-    return;
 }
