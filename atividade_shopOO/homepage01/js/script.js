e79cf323b576877507c9dcc67f9fdc231806d927
RUBENSNETO0001 <rubensnetomartinssuarezneto@gmail.com>
2024-10-30 09:45:50 -0500

    versão02

--- atividade_shopOO/homepage01/js/script.js
+++
@@ -1,6 +0,0 @@
-const btn = document.querySelector("#btnMenu");
-const item = document.querySelectorAll("itens");
-
-btn.forEach("click", ()=> {
-    item.style.display = "block"? "none":"block"
-});
